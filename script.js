a855d962ebac8ca3a869e4a9b26f32789c5dd282
chrislopez93 <sinretel@gmail.com>
2023-07-18 23:43:01 -0400

    Ejercicio finalizado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function test(){
     let pedazo = document.querySelector(".pedazo").value;
     let personas = document.querySelector(".personas").value;
     let mensaje = document.querySelector(".mensaje")
-    let cantidad = pedazo / personas;
+    let cantidad = pedazo / personas; /*Si hay más pedazos que personas*/
     if (cantidad >= 1){
         mensaje = document.querySelector('.mensaje');
         mensaje.innerHTML = "Hay suficiente";
