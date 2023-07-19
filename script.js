d89d2fd5e5b25277d9467cf1248749fe1fe05101
chrislopez93 <sinretel@gmail.com>
2023-07-18 23:40:00 -0400

    Ejercicio finalizado

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function test(){
-    const pedazo = document.getElementById('#pedazo').value;
-    const personas = document.getElementById('#personas').value;
+    let pedazo = document.querySelector(".pedazo").value;
+    let personas = document.querySelector(".personas").value;
+    let mensaje = document.querySelector(".mensaje")
     let cantidad = pedazo / personas;
-    if (cantidad => 1){
-        mensaje = documento.getElementById('.mensaje');
+    if (cantidad >= 1){
+        mensaje = document.querySelector('.mensaje');
         mensaje.innerHTML = "Hay suficiente";
     }
     else{
+        mensaje = document.querySelector('.mensaje');
         mensaje.innerHTML ="No hay suficiente";
     }
 
