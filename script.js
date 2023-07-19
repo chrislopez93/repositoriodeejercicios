59958d82214816b63da9314ad32945d01e734c33
chrislopez93 <sinretel@gmail.com>
2023-07-18 23:52:57 -0400

    línea de código en script variable mensaje eliminado

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function test(){
     let pedazo = document.querySelector(".pedazo").value;
     let personas = document.querySelector(".personas").value;
-    let mensaje = document.querySelector(".mensaje")
     let cantidad = pedazo / personas; /*Si hay más pedazos que personas*/
     if (cantidad >= 1){
         mensaje = document.querySelector('.mensaje');
